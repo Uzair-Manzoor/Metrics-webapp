9ab6b50f6c9c2fe62c53073d79abf413dca6cf98
Uzair-Manzoor <uzair.kayani555@gmail.com>
2023-10-11 02:59:13 +0500

    Error Handling in States Component

--- src/components/States.js
+++ src/components/States.js
@@ -5,16 +5,23 @@ import { API_BASE_URL, API_KEY } from '../api';
 const States = () => {
   const [states, setStates] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const fetchStates = async () => {
     try {
+      setError(null);
+      setLoading(true);
       const response = await fetch(
         `${API_BASE_URL}/states?country=Canada&key=${API_KEY}`,
       );
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
       const data = await response.json();
       setStates(data.data);
       setLoading(false);
     } catch (error) {
+      setError(error);
       setLoading(false);
     }
   };
@@ -23,25 +30,37 @@ const States = () => {
     fetchStates();
   }, []);
 
+  const renderStates = () => {
+    if (loading) {
+      return <p>Loading...</p>;
+    }
+    if (error) {
+      return (
+        <div>
+          <p>Error fetching data. Please try again.</p>
+          <button type="button" onClick={fetchStates}>
+            Retry
+          </button>
+        </div>
+      );
+    }
+    return (
+      <ul className="items">
+        {states.map((state) => (
+          <li key={state.state}>
+            <Link to={`/${state.state}`} className="item-link">
+              {state.state}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <div>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <ul className="items">
-          {Array.isArray(states) && states.length > 0 ? (
-            states.map((state) => (
-              <li key={state.state}>
-                <Link to={`/${state.state}`} className="item-link">
-                  {state.state}
-                </Link>
-              </li>
-            ))
-          ) : (
-            <p>No states available.</p>
-          )}
-        </ul>
-      )}
+      <h2>Canadian States</h2>
+      {renderStates()}
     </div>
   );
 };
